89662210c79c53a15f9560a370bf5d2ea5169b11
Wiktor Markowski <wiktor.markowski@student.put.poznan.pl>
2021-05-15 22:19:13 +0100

    Code refactoring

--- Gun.h
+++ Gun.h
@@ -6,7 +6,8 @@ class Gun :
 
 
 public:
-    Gun(int x, int y, int &t) {
+    Gun(int x, int y, int &t) 
+    {
         _Timer = &t;
         _Texture.loadFromFile("grafiki/gun-1.png");
         _Sprite.setTexture(_Texture);
@@ -20,52 +21,57 @@ public:
         _RangeCircle.setOrigin(_Range, _Range);
         _RangeCircle.setPosition(x, y);
     }
-    ~Gun() {
-
-    }
-
-    void draw(RenderWindow& window) {
+    void draw(RenderWindow& window) 
+    {
         window.draw(_Sprite);
         window.draw(_RangeCircle);
         _CanShoot = true;
         _Cost = 1 + (_TowerLevel - 1) * 15;
     }
-    int getLvl() {
+    int getLvl() 
+    {
         return _TowerLevel;
     }
-    int getType() {
+    int getType() 
+    {
         return _Type;
     }
-    int getCost() {
+    int getCost() 
+    {
         return _Cost;
     }
-    void upgrade() {
+    void upgrade() 
+    {
         _TowerLevel++;
         _Dmg += _TowerLevel * _Dmg;
         _Range += 50;
         _AttackSpeed += 0.2;
     }
-    Sprite getSprite() {
+    Sprite getSprite() 
+    {
         return _Sprite;
     }
-    bool collision(Vector2f v) {
-        
+    bool collision(Vector2f v) 
+    {
         if (_Sprite.getGlobalBounds().contains(v)) return true;
         else return false;
     }
-    void setPosition(Vector2f p) {
+    void setPosition(Vector2f p) 
+    {
         _Sprite.setPosition(p);
-       
     }
-    void shoot(SzczerbiakBagienny& mob, Vector2f p, list<Bullet*>& b) {
-        if (_RangeCircle.getGlobalBounds().contains(p)) {
-            
-            if (_CanShoot) {
-                if ((*_Timer % int(60 / _AttackSpeed) == 0)) {
+    void shoot(SzczerbiakBagienny& mob, Vector2f p, list<Bullet*>& b) 
+    {
+        if (_RangeCircle.getGlobalBounds().contains(p)) 
+        {
+            if (_CanShoot) 
+            {
+                if ((*_Timer % int(60 / _AttackSpeed) == 0)) 
+                {
                     Vector2f AimDir = mob.getPos() - _Sprite.getPosition();
                     Vector2f AimDirNorm = Vector2f(AimDir.x / sqrt(pow(AimDir.x, 2) + pow(AimDir.y, 2)), AimDir.y/ sqrt(pow(AimDir.x, 2) + pow(AimDir.y, 2)));
                     float angle = atan2(AimDir.y, AimDir.x) * 180 / 3.14;
-                    
+                 
                     _Sprite.setRotation(angle);
 
                     Bullet* bullet = new Bullet(mob, _Sprite.getPosition(), AimDirNorm, *_Timer, _Type, _Dmg);
@@ -77,7 +83,6 @@ public:
     }
 
 private:
-
     bool _CanShoot = true;
     int* _Timer;
     int _Cost = 10;
@@ -89,6 +94,5 @@ private:
     CircleShape _RangeCircle;
     int _Dmg = 10;
     float _AttackSpeed = 3.0;
-
 };
 
